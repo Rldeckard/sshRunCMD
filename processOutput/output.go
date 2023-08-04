2bf65d7277a3323229997fc3dd660f6921071b5f
Deckard <30917551+Rldeckard@users.noreply.github.com>
2023-08-04 14:34:11 -0400

    broke out more functions to improve readability

---
+++ processOutput/output.go
@@ -0,0 +1,28 @@
+package output
+
+import (
+	"strings"
+)
+
+// Removes the banners from the output array to make the code easier to digest.
+// Also looks for any errors in the execution.
+func Process(input []string, originalOutput bool) ([]string, bool) {
+	failedCommand := false
+	for index, bannerString := range input {
+		if originalOutput == false {
+			if strings.Contains(bannerString, "-------------------------------") {
+				input[index-1] = ""
+				input[index] = ""
+				input[index+1] = ""
+			}
+			if strings.Contains(bannerString, "terminal length") {
+				input[index] = ""
+			}
+		}
+		if strings.Contains(bannerString, "% Invalid") {
+			failedCommand = true
+		}
+	}
+	return input, failedCommand
+
+}
